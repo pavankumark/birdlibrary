6f86ef82ede4c7b15bd2a14bb4cd1049da3538f2
Pavan Kumar <pavan@reachall.in>
2015-07-04 01:49:08 +0530

    handling invalid calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActionController::RoutingError, :with => :render_404
+
+  def render_404
+    render json: {warn: "Invalid call .. Dont try to play with"}, status: 404
+  end
+
+  def raise_not_found!
+    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 BirdLibrary::Application.routes.draw do
 
   resources :birds, :only => [:index, :create, :show, :destroy]
+  root :to => 'application#raise_not_found!'
+
+  match '*unmatched_route', :to => 'application#raise_not_found!'
 
 end
